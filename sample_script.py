e59c5d33f98a967f4482946e79b0bcf797f09a53
kfriden <kfriden@verizon.net>
2024-03-22 16:57:41 -0400

    Updated automated search script with new Google search term

--- sample_script.py
+++ sample_script.py
@@ -18,16 +18,16 @@ driver.get('https://www.google.com/')
 # populate search field
 search = driver.find_element(By.NAME, 'q')
 search.clear()
-search.send_keys('Car')
+search.send_keys('Tighnari')
 
-# wait for 4 sec
-sleep(4)
+# wait for 3 sec
+sleep(3)
 
 # click search button
 driver.find_element(By.NAME, 'btnK').click()
 
 # verify search results
-assert 'car' in driver.current_url.lower(), f"Expected query not in {driver.current_url.lower()}"
+assert 'tighnari' in driver.current_url.lower(), f"Expected query not in {driver.current_url.lower()}"
 print('Test Passed')
 
 driver.quit()
